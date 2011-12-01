58d6c86734274a06a88a415c1b5e4c85f7b41b8a
seph <seph@directionless.org>
2011-12-01 14:42:53 -0500

    Finish off the passenger apache2 support

--- providers/apache2.rb
+++ providers/apache2.rb
@@ -11,24 +11,37 @@ action :create do
     creates passenger_module_path(new_resource.gem, new_resource.version)
   end
 
-  # Generate the apache snippet by asking passenger to do it for us.
-  # This is a little weird, since it will use the first ruby on your
-  # path, *not* the one it was compiled with. (WTF poassenger)
-  # Luckily, it looks like we can just call it with an explicit ruby
-  # to get the desired behavior.
-  template "#{node[:apache][:dir]}/mods-available/passenger.load" do
-    cookbook "passenger"
-    source "apache.load.erb"
-    owner "root"
-    group "root"
-    mode 0644
-    variables( 
-              :bin => [
-                       passenger_find_ruby_path(new_resource.gem, new_resource.version),
-                       passenger_find_executable("passenger-install-apache2-module", new_resource.gem, new_resource.version),
-                      ].join(" ")
-              )
-    only_if do ::File.exists?(passenger_find_root_path(new_resource.gem, new_resource.version) ) end
+  if ::File.exists?(passenger_find_root_path(new_resource.gem, new_resource.version) )
+
+    # Generate the apache snippet by asking passenger to do it for us.
+    # This is a little weird, since it will use the first ruby on your
+    # path, *not* the one it was compiled with. (WTF poassenger)
+    # Luckily, it looks like we can just call it with an explicit ruby
+    # to get the desired behavior.
+    template "#{node[:apache][:dir]}/mods-available/passenger.load" do
+      cookbook "passenger"
+      source "apache.load.erb"
+      owner "root"
+      group "root"
+      mode 0644
+      variables( 
+                :bin => [
+                         passenger_find_ruby_path(new_resource.gem, new_resource.version),
+                         passenger_find_executable("passenger-install-apache2-module", new_resource.gem, new_resource.version),
+                        ].join(" ")
+                )
+    end
+
+    template "#{node[:apache][:dir]}/mods-available/passenger.conf" do
+      cookbook "passenger"
+      source "apache.conf.erb"
+      owner "root"
+      group "root"
+      mode 0644
+    end
+
+    apache_module "passenger"
+
   end
 
 end
