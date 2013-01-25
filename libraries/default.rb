a80201eb1819067e9adddfda7b00de0ca62d95a2
Decklin Foster <decklin@red-bean.com>
2013-01-25 14:21:38 -0500

    More defensive passenger searching

--- libraries/default.rb
+++ libraries/default.rb
@@ -3,6 +3,8 @@
 # tell is is the gem path. These functions will start with a gem
 # command, and find the desired things.
 
+# These may fall through to returning nil.
+
 #class Chef
 #  class Recipe
 
@@ -11,14 +13,13 @@
       # sometimes gem returns an absolute path, sometime it does
       # not. So we're going to canonicalize it.
       version_arg = version.nil? ? "" : "--version '#{version}'"
-      bin = `#{gem} contents passenger #{version_arg}`.split("\n").select { |i| i.match("bin/#{executable}$") }.first
-      if bin.nil?
-        return nil
-      elsif bin.match("^/")
-        return bin
-      else
-        gemdir = `#{gem} environment`.split("\n").map { |i| i.gsub!(/.*INSTALLATION DIRECTORY:\s*/, "") }.compact
-        return ::File.join(gemdir, bin)
+      if bin = `#{gem} contents passenger #{version_arg}`.split("\n").select { |i| i.match("bin/#{executable}$") }.first
+        if bin.match("^/")
+          return bin
+        else
+          gemdir = `#{gem} environment`.split("\n").map { |i| i.gsub!(/.*INSTALLATION DIRECTORY:\s*/, "") }.compact
+          return ::File.join(gemdir, bin)
+        end
       end
     end
 
@@ -28,8 +29,9 @@
 
 
     def passenger_find_root_path(gem="/usr/bin/gem", version=nil)
-      pc = passenger_find_executable("passenger-config", gem, version)
-      `#{pc} --root`.chomp
+      if pc = passenger_find_executable("passenger-config", gem, version)
+        `#{pc} --root`.chomp
+      end
     end
 
     def passenger_module_path(gem="/usr/bin/gem", version=nil)
