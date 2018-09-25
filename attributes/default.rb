b54be811360c6ad165ae8cdb04d33e655efa669d
David Fisher <david@actbluetech.com>
2018-09-25 12:53:49 -0400

    Stop Passenger preloader from idle
    When combined with a non-zero MaxRequests setting this results
    in the preloader potentially shutting down / idling, and semi-frequent
    forks happening slowly that can cause 503 errors that we've been
    seeing.
    
    Holding off on changing MaxRequests to zero as well since we are at
    a big EOQ during midterms, but after that the recommendation is to
    set MaxRequests to zero as well, plus overall tune Passenger and
    upgrade it to a newer version.
    
    Hoping to observe 503s drastically reduced in production after this
    is converged.
    
    There is a Google Docs presentation by David Fisher covering the process
    at: https://docs.google.com/presentation/d/1CSn5IzghkRBAc1Rfx5yFI3r-9_AajE9YD6d05oMcWKQ

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,16 @@
 # config settings
 default[:passenger][:conf][:max_pool_size] = 10
 default[:passenger][:conf][:max_requests] = 20000
+
+# Prevent the preloader from shutting down.
+# Default value is 300 (seconds) / 5 minutes
+# Non-zero values useful to save memory on shared servers
+# but less useful on dedicated application servers like Indigo
+# Non-zero values combined with non-zero MaxRequests settings can result
+# in slow forking since the preloader could be shut down when the
+# MaxRequests restart/fork happens
+default[:passenger][:conf][:max_preloader_idle_time] = 0
+
 default[:passenger][:conf][:min_instances] = 2
 default[:passenger][:conf][:max_instances_per_app] = 0
 #default[:passenger][:conf][:pool_idle_time]
--- metadata.rb
+++ metadata.rb
@@ -1,3 +1,3 @@
 name 'passenger'
 description 'passenger'
-version '0.0.2'
+version '0.0.3'
